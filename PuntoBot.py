84fb87e062de8029267addf8cd76998e9fb3c243
Kyle Coop <kylecoop152@gmail.com>
2020-11-21 00:41:39 +0000

    added new command

--- PuntoBot.py
+++ PuntoBot.py
@@ -1,47 +1,46 @@
-import random
 import asyncio
-import aiohttp
-import rule34
-import discord
+import random
+import html
 from os import getenv
-from dotenv import load_dotenv
-from requests import get
-from json import loads
-from html import escape
-from discord import Game
+
+import discord
 from discord.ext.commands import Bot
+from dotenv import load_dotenv
+import requests
+from rule34 import Rule34
 
 PREFIX = "##"
 load_dotenv()
 TOKEN = getenv('DISCORD_TOKEN')
-#Get at discordapp.com/developers/applications/me
+# Get at discordapp.com/developers/applications/me
 client = Bot(command_prefix=PREFIX)
 client.remove_command("help")
 
-###RULE34 GRABBER
-rule34 = rule34.Rule34(asyncio.get_event_loop())
+# RULE34 GRABBER
+rule34 = Rule34(asyncio.get_event_loop())
 
-previous = None
 
-@client.command(name = '8ball',
-                description = "Answers from the great beyond. (Answers a yes/no question.)",
-                aliases = ['eight_ball', 'eightball', '8-ball'])
+@client.command(name='8ball',
+                description="Answers from the great beyond. (Answers a yes/no question.)",
+                aliases=['eight_ball', 'eightball', '8-ball'])
 async def eight_ball(ctx):
     with open("8ball.txt", "r") as file:
         responses = file.readlines()
     await ctx.send(f"{random.choice(responses)}, {ctx.message.author.mention}")
 
-@client.command(name = "power",
-                description = "Totally exponential! Used: power [number] [power]")
-async def power(ctx, number, power):
+
+@client.command(name="exp",
+                description="Totally exponential! Used: exp [number] [power]")
+async def exp(ctx, number, power):
     try:
         pow_value = float(number) ** float(power)
         await ctx.send(f"{number} to the power of {power} is {pow_value}.")
-    except:
+    except ValueError:
         await ctx.send("Not a number!")
 
-@client.command(name = "r34",
-                description = "used by war criminals to locate cheese pizza. Used: r34 [keyword]")
+
+@client.command(name="r34",
+                description="used by war criminals to locate cheese pizza. Used: r34 [keyword]")
 async def r34(ctx, *args):
     keyword = ' '.join(args)
     print("getting r34\nkeyword = " + keyword)
@@ -54,75 +53,84 @@ async def r34(ctx, *args):
         await message.add_reaction("\U0001F6AB")
         try:
             await client.wait_for("message",
-                                  check = lambda r : r.content in ("tags", "tags?", "tag", "tag?"))
+                                  check=lambda r: r.content in ("tags", "tags?", "tag", "tag?"))
             message = await ctx.send(f"tags: `{', '.join(choice.tags)}`")
             await message.add_reaction("\U0001F6AB")
         except asyncio.TimeoutError:
             print("nobody wanted tags for " + choice.file_url)
-       
-@client.command(name = "e621",
-                description = "used by me to locate cheese graters. Used: e621 [keyword(s)]")
+
+
+@client.command(name="e621",
+                description="used by me to locate cheese graters. Used: e621 [keyword(s)]")
 async def e621(ctx, *args):
     url = "https://www.e621.net/posts.json?limit=100&tags="
-    keyword = '+'.join([escape(arg) for arg in args])
+    keyword = '+'.join([html.escape(arg) for arg in args])
     url += keyword
     print("getting e621\nkeyword = " + keyword)
     headers = {'User-Agent': 'cute152DiscordBot'}
-    resp = get(url, headers = headers)
-    parsed = loads(resp.text)
-    if parsed['posts']: #if list not empty
+    resp = requests.get(url, headers=headers)
+    parsed = resp.json()
+    if parsed['posts']:  # if list not empty
         choice = random.choice(parsed['posts'])
         message = await ctx.send(choice['file']['url'])
-        await message.add_reaction("\U0001F6AB") # add the delete reaction
+        await message.add_reaction("\U0001F6AB")  # add the delete reaction
     else:
         await ctx.send("No results.")
 
-@client.command(name = "e926",
-                description = "how nice. Used: e926 [keyword(s)]")
+
+@client.command(name="e926",
+                description="how nice. Used: e926 [keyword(s)]")
 async def e926(ctx, *args):
     await e621(ctx, "rating:safe", *args)
 
-@client.command(name = "help",
-                description = "displays a list of all commands.")
-async def help(ctx):
-    helpMessage = discord.Embed(title = "Help", color=0xFF8000)
-    helpMessage.set_author(name="Jeffrey Epstein", url="https://google621.neocities.org")
-    helpMessage.set_thumbnail(url="https://pbs.twimg.com/profile_images/1315716250706882563/Z1eDpiVY_400x400.jpg")
-    for command in client.commands:
-        helpMessage.add_field(name = PREFIX + command.name,
-                              value = command.description if command.description else "[no description added]",
-                              inline = False)
-    await ctx.send(embed = helpMessage)
 
-@client.command(name = "pyramid",
-                description = "create a sick awesome legit pyramid of teh ultamte dooom!!!!!!1!11 Used: pyramid [levels]")
+@client.command(name="help",
+                description="displays a list of all commands.")
+async def help(ctx):
+    help_message = discord.Embed(title="Help", color=0xFF8000)
+    help_message.set_author(name="Jeffrey Epstein", url="https://google621.neocities.org")
+    help_message.set_thumbnail(url="https://pbs.twimg.com/profile_images/1315716250706882563/Z1eDpiVY_400x400.jpg")
+    for command in client.commands:
+        help_message.add_field(name=PREFIX + command.name,
+                               value=command.description if command.description else "[no description added]",
+                               inline=False)
+    await ctx.send(embed=help_message)
+
+
+@client.command(name="pyramid",
+                description="create a sick awesome legit pyramid of teh ultamte dooom!!!!!!1!11 Used: pyramid [levels]")
 async def pyramid(ctx, levels_str):
-    try: levels = int(levels_str)
-    except: await ctx.send("that isnt a number")
-    pyramid = "```"
-    width = (levels * 2) - 1
-    for x in range(1, levels + 1):
-        pyramid += " " * (levels - x)
-        pyramid += "* " * x
-        pyramid += " " * (levels - (x + 1))
-        pyramid += "\n"
-    pyramid += "```"
-    await ctx.send(pyramid)
+    try:
+        levels = int(levels_str)
+        result = "```"
+        for x in range(1, levels + 1):
+            result += " " * (levels - x)
+            result += "* " * x
+            result += " " * (levels - (x + 1))
+            result += "\n"
+        result += "```"
+        await ctx.send(result)
+    except ValueError:
+        await ctx.send("That isn't a number")
+
+@client.command(name="imitate",
+                description="can you say that again?")
+def imitate(ctx: discord.ext.commands.Context):
+    await ctx.send(ctx.message.content)
+
 
 @client.event
 async def on_reaction_add(reaction, user):
     if not user.bot and reaction.emoji == "\U0001F6AB":
         await reaction.message.delete()
 
-#SET BOT STATE
+
+# SET BOT STATE
 @client.event
 async def on_ready():
     print("Trying to set presence... ")
-    await client.change_presence(activity=Game(name="gaming, like a boss"))
+    await client.change_presence(activity=discord.Game(name="gaming, like a boss"))
     print("Logged in as " + client.user.name)
 
-#@client.event
-#async def on_command_error(ctx, error):
-#    await ctx.send("you typed it wrong, brainlet!")
 
 client.run(TOKEN)
