0eacfd17c852703893a7e6e453608e16398e1f86
Kyle Coop <kylecoop152@gmail.com>
2021-04-19 18:40:59 +0100

    yrdyutrdytrd

--- PuntoBot.py
+++ PuntoBot.py
@@ -51,7 +51,7 @@ async def r34(ctx, *args):
     else:
         choice = random.choice(results)
         message = await ctx.send(choice.file_url)
-		log_user_request(ctx.user, message)
+        log_user_request(ctx.user, message)
         await message.add_reaction("\U0001F6AB")
         try:
             await client.wait_for("message",
@@ -75,7 +75,7 @@ async def e621(ctx, *args):
     if parsed['posts']:  # if list not empty
         choice = random.choice(parsed['posts'])
         message = await ctx.send(choice['file']['url'])
-		log_user_request(ctx.user, message)
+        log_user_request(ctx.user, message)
         await message.add_reaction("\U0001F6AB")  # add the delete reaction
     else:
         await ctx.send("No results.")
