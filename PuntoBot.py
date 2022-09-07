740793f30f12e9e9710c831b512af897b6105cb1
Kyle Coop <kylecoop152@gmail.com>
2022-09-07 18:40:17 +0100

    Remove unfinished pokesmash assets now purely a discord.py version update

--- PuntoBot.py
+++ PuntoBot.py
@@ -15,12 +15,11 @@ class PuntoBot(Bot):
         await bot.load_extension("errorreporting")
         await bot.load_extension("image_getters")
         await bot.load_extension("generators")
-        await bot.load_extension("smashorpass")
         await super().setup_hook()
 
 
 if TOKEN is None:
-    print("The token isn't in .env or the environment variables.")
+    print("The token isn't in .env or the environment variables.\nAdd it with the name 'DISCORD_TOKEN'.")
 else:
     # idk how intents work LMAO!!!!!
     bot = PuntoBot(command_prefix=PREFIX, intents=Intents.all())
